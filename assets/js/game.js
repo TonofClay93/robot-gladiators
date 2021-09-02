9263a32aeba05457d93fa2d9c70f604239b76275
TonofClay93 <88801144+TonofClay93@users.noreply.github.com>
2021-09-02 11:12:36 -0400

    enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,12 +5,12 @@ var playerMoney = 10;
 
 // You can also log multiple values at once like  this console.log(playerName, player Attack)
 
-var enemyName = "Roborto the Roboto"
+var enemyNames = ["Roborto", "Amy Andriod", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 //create function
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
@@ -62,4 +62,6 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   }
 }
 //execute function
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
